4bba2d2f726ca76d573b053241a7e277e46be4a9
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-06-29 00:42:40 -0400

    feat: add 'new-order' socket to listen for and emit new orders.

--- models/sockets.js
+++ models/sockets.js
@@ -11,10 +11,17 @@ class Sockets {
     socketEvents() {
         // On connection
         this.io.on('connection', (socket) => {
+            console.log('Conectado', socket.id);
+
             // Escuchar evento: mensaje-to-server
             socket.on('mensaje-to-server', (data) => {
                 console.log(data);
                 this.io.emit('mensaje-from-server', data);
+            });
+
+            socket.on('new-order', (data) => {
+                console.log({ data });
+                this.io.emit('new-order', data);
             });
 
         });
